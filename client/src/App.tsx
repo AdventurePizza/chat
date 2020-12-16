7e8edc56a5ddd51108f8d13c5422e0e62cecbe27
Tonymndz <Tonymndz1141@gmail.com>
2020-12-16 15:26:37 -0700

    Moved & Deleted some sounds logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import {
 	IFigure,
 	IGifs,
 	IMessageEvent,
-	ISound,
 	IUserLocations,
 	IUserProfiles,
 	PanelItemEnum
@@ -29,42 +28,12 @@ import { Panel } from './components/Panel';
 import { UserCursor } from './components/UserCursors';
 import _ from 'underscore';
 //@ts-ignore
-import ahh from './assets/sounds/funny/ahh.mp3';
+import audioEnter from './assets/sounds/zap-enter.mp3';
 //@ts-ignore
-import air from './assets/sounds/funny/air.mp3';
-//@ts-ignore
-import applause from './assets/sounds/funny/applause.mp3';
-// Sound imports
-//@ts-ignore
-import audioEnter from './assets/sounds/zap-enter.mp3'; //@ts-ignore
-import audioExit from './assets/sounds/zap-exit.mp3'; //@ts-ignore
-// Nature
-import bee from './assets/sounds/nature/bee.mp3'; //@ts-ignore
-import clang from './assets/sounds/funny/clang.mp3'; //@ts-ignore
-// Instruments
-import cymbalHit from './assets/sounds/instruments/cymbal.mp3'; //@ts-ignore
-import dog from './assets/sounds/nature/dog.mp3'; //@ts-ignore
-import drumBeat from './assets/sounds/instruments/drumbeat.mp3'; //@ts-ignore
-import flying_fox from './assets/sounds/nature/flying-fox.mp3'; //@ts-ignore
-import gong from './assets/sounds/instruments/chinese-gong.wav'; //@ts-ignore
-// Funny
-import gotEm from './assets/sounds/funny/ha-got-eeem.mp3'; //@ts-ignore
-import groan from './assets/sounds/funny/groan.mp3'; //@ts-ignore
-import guitarStrum from './assets/sounds/instruments/electric_guitar.mp3'; //@ts-ignore
-import harp from './assets/sounds/instruments/harp.wav'; //@ts-ignore
-import horn from './assets/sounds/funny/horn.mp3'; //@ts-ignore
+import audioExit from './assets/sounds/zap-exit.mp3';
 import io from 'socket.io-client';
-//@ts-ignore
-import laugh from './assets/sounds/funny/laugh.mp3'; //@ts-ignore
-import lightning from './assets/sounds/nature/lightning.mp3'; //@ts-ignore
-import nature from './assets/sounds/nature/nature.mp3'; //@ts-ignore
-import noice from './assets/sounds/funny/noice.mp3'; //@ts-ignore
-import sealion from './assets/sounds/nature/sealion.mp3'; //@ts-ignore
-import stop_it_get_some_help from './assets/sounds/funny/stop_it_get_some_help.mp3'; //@ts-ignore
-import trumpet from './assets/sounds/instruments/trumpet.mp3'; //@ts-ignore
 import { v4 as uuidv4 } from 'uuid';
-//@ts-ignore
-import water from './assets/sounds/nature/water.mp3';
+import { cymbalHit, sounds } from './components/Sounds';
 
 const isDebug = false;
 
@@ -79,35 +48,6 @@ const socket = io(socketURL, { transports: ['websocket'] });
 
 const API_KEY = 'A7O4CiyZj72oLKEX2WvgZjMRS7g4jqS4';
 const GIF_FETCH = new GiphyFetch(API_KEY);
-
-const sounds: ISound = {
-	// Instrument
-	drum: drumBeat,
-	cymbal: cymbalHit,
-	guitar: guitarStrum,
-	trumpet: trumpet,
-	gong: gong,
-	harp: harp,
-	// Funny
-	meme: gotEm,
-	noice: noice,
-	stop_it: stop_it_get_some_help,
-	ahh: ahh,
-	air: air,
-	applause: applause,
-	groan: groan,
-	clang: clang,
-	horn: horn,
-	laugh: laugh,
-	// Nature
-	bee: bee,
-	dog: dog,
-	flying_fox: flying_fox,
-	lightning: lightning,
-	nature: nature,
-	sealion: sealion,
-	water: water
-};
 
 function App() {
 	const [isPanelOpen, setIsPanelOpen] = useState(true);
@@ -174,83 +114,7 @@ function App() {
 
 	const playSound = useCallback(
 		(soundType) => {
-			switch (soundType) {
-				// Instrument
-				case 'drum':
-					audio.current = new Audio(sounds.drum);
-					break;
-				case 'cymbal':
-					audio.current = new Audio(sounds.cymbal);
-					break;
-				case 'guitar':
-					audio.current = new Audio(sounds.guitar);
-					break;
-				case 'trumpet':
-					audio.current = new Audio(sounds.trumpet);
-					break;
-				case 'gong':
-					audio.current = new Audio(sounds.gong);
-					break;
-				case 'harp':
-					audio.current = new Audio(sounds.harp);
-					break;
-				// Funny
-				case 'meme':
-					audio.current = new Audio(sounds.meme);
-					break;
-				case 'noice':
-					audio.current = new Audio(sounds.noice);
-					break;
-				case 'stop_it':
-					audio.current = new Audio(sounds.stop_it);
-					break;
-				case 'ahh':
-					audio.current = new Audio(sounds.ahh);
-					break;
-				case 'air':
-					audio.current = new Audio(sounds.air);
-					break;
-				case 'applause':
-					audio.current = new Audio(sounds.applause);
-					break;
-				case 'groan':
-					audio.current = new Audio(sounds.groan);
-					break;
-				case 'clang':
-					audio.current = new Audio(sounds.clang);
-					break;
-				case 'horn':
-					audio.current = new Audio(sounds.horn);
-					break;
-				case 'laugh':
-					audio.current = new Audio(sounds.laugh);
-					break;
-				// Nature
-				case 'bee':
-					audio.current = new Audio(sounds.bee);
-					break;
-				case 'dog':
-					audio.current = new Audio(sounds.dog);
-					break;
-				case 'flying_fox':
-					audio.current = new Audio(sounds.flying_fox);
-					break;
-				case 'lightning':
-					audio.current = new Audio(sounds.lightning);
-					break;
-				case 'nature':
-					audio.current = new Audio(sounds.nature);
-					break;
-				case 'sealion':
-					audio.current = new Audio(sounds.sealion);
-					break;
-				case 'water':
-					audio.current = new Audio(sounds.water);
-					break;
-				default:
-					// This should be impossible
-					break;
-			}
+      audio.current = new Audio(sounds[soundType])
 
 			if (!audio || !audio.current) return;
 
@@ -264,32 +128,7 @@ function App() {
 			audio.current.currentTime = 0;
 			audio.current.play();
 		},
-		[
-			audio,
-			sounds.drum,
-			sounds.cymbal,
-			sounds.guitar,
-			sounds.trumpet,
-			sounds.gong,
-			sounds.harp,
-			sounds.meme,
-			sounds.noice,
-			sounds.stop_it,
-			sounds.ahh,
-			sounds.air,
-			sounds.applause,
-			sounds.groan,
-			sounds.clang,
-			sounds.horn,
-			sounds.laugh,
-			sounds.bee,
-			sounds.dog,
-			sounds.flying_fox,
-			sounds.lightning,
-			sounds.nature,
-			sounds.sealion,
-			sounds.water
-		]
+		[]
 	);
 
 	const onClickPanelItem = (key: string) => {
