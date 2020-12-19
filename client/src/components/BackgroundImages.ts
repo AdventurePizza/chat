d8c7318330081f076bb21af236b8ef201e638328
Tonymndz <Tonymndz1141@gmail.com>
2020-12-19 12:51:54 -0700

    Background Panel w/Changes

---
+++ client/src/components/BackgroundImages.ts
@@ -0,0 +1,26 @@
+import { IBackgrounds } from '../types';
+import butterflys from '../assets/backgrounds/butterflys.jpg';
+import grey_board from '../assets/backgrounds/grey_board.jpg';
+import ice from '../assets/backgrounds/ice.jpg';
+import mountain from '../assets/backgrounds/mountain.jpg';
+import nature from '../assets/backgrounds/nature.jpg';
+import night_sky from '../assets/backgrounds/night_sky.jpg';
+import stones from '../assets/backgrounds/stones.jpg';
+import tiles from '../assets/backgrounds/tiles.jpg';
+import tree from '../assets/backgrounds/tree.jpg';
+import triangles from '../assets/backgrounds/triangles.png';
+
+export const backgroundIcons: IBackgrounds = {
+	butterflys: butterflys,
+	grey_board: grey_board,
+	ice: ice,
+	mountain: mountain,
+	nature: nature,
+	night_sky: night_sky,
+  stones: stones,
+  tree: tree,
+	tiles: tiles,
+	triangles: triangles
+};
+
+export const backgrounds: IBackgrounds = { ...backgroundIcons, none: "none" };
---
+++ client/src/components/BackgroundPanel.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { IconButton, Avatar } from '@material-ui/core';
+import { backgroundIcons } from './BackgroundImages';
+
+interface IBackgroundPanelProps {
+	sendBackground: (backgroundText: string, backgroundType: string) => void;
+}
+
+const BackgroundPanel = ({ sendBackground }: IBackgroundPanelProps) => {
+	const displayIcons = Object.keys(backgroundIcons).map(backgroundName => {
+		const backgroundIcon = backgroundIcons[backgroundName];
+		return (
+			<IconButton
+				key={backgroundName}
+				onClick={() => sendBackground('background', backgroundName)}
+			>
+				<Avatar
+					variant="rounded"
+					src={backgroundIcon}
+					alt={backgroundName + ' background'}
+				/>
+			</IconButton>
+		);
+	});
+
+	return (
+		<div className="background-container">
+			<div className="background-icon-list">{displayIcons}</div>
+		</div>
+	);
+};
+
+export default BackgroundPanel;
