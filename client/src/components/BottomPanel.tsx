1b4b4248466dec08b4ac43d146e7f0af33ed9dc2
Tonymndz <tonymndz1141@gmail.com>
2021-01-11 20:50:16 -0700

    Changes to background panel & Bottom panel (#39)
    * Changes to background panel & Bottom panel
    
    - Made BottomPanel & Background Panel no longer define components inside a component
    
    - On entering the background panel for the first time, it will auto-search for trending images
    
    - Once you leave the background panel, the images you previously had are now still there
    
    * reduce code redundancy for icon props
    
    * prettier
    
    * v1.18.0

--- client/src/components/BottomPanel.tsx
+++ client/src/components/BottomPanel.tsx
@@ -1,12 +1,12 @@
 import { Drawer, IconButton } from '@material-ui/core';
 import { ITowerDefenseState, PanelItemEnum } from '../types';
-
+import { IImagesState } from './BackgroundPanel';
 import AnimationPanel from './AnimationPanel';
 import BackgroundPanel from './BackgroundPanel';
 import { Chat } from './Chat';
 import { Gifs } from './Gifs';
 import { IGif } from '@giphy/js-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { SettingsPanel } from './SettingsPanel';
 import SoundPanel from './SoundPanel';
 import { TowerDefensePanel } from './TowerDefensePanel';
@@ -21,6 +21,16 @@ export interface IPanelProps {
 	onAction: (key: string, ...args: any[]) => void;
 	towerDefenseState: ITowerDefenseState;
 	updateIsTyping: (isTyping: boolean) => void;
+}
+
+export interface IPanelContentProps {
+	type?: PanelItemEnum;
+	setBrushColor: (color: string) => void;
+	onAction: (key: string, ...args: any[]) => void;
+	towerDefenseState: ITowerDefenseState;
+	updateIsTyping: (isTyping: boolean) => void;
+	images: IImagesState[];
+	setImages: React.Dispatch<React.SetStateAction<IImagesState[]>>;
 }
 
 export interface ISoundPairs {
@@ -40,88 +50,112 @@ export const BottomPanel = ({
 	towerDefenseState,
 	updateIsTyping
 }: IPanelProps) => {
-	const renderPanelContent = () => {
-		switch (type) {
-			case 'emoji':
-				return (
-					<>
-						{emojiList.map((emoji) => (
-							<div key={emoji} className="bottom-panel-emoji">
-								<IconButton onClick={() => onAction('emoji', emoji)}>
-									{emoji}
-								</IconButton>
-							</div>
-						))}
-					</>
-				);
-
-			case 'chat':
-				return (
-					<Chat
-						sendMessage={(message) => {
-							onAction('chat', message);
-						}}
-						updateIsTyping={updateIsTyping}
-					/>
-				);
-			case 'sound':
-				return <SoundPanel sendSound={onAction} />;
-
-			case 'gifs':
-				return (
-					<Gifs
-						sendGif={(gif: IGif) => {
-							onAction('gif', gif.id);
-						}}
-					/>
-				);
-			case 'tower':
-				return (
-					<TowerDefensePanel
-						isStarted={towerDefenseState.isPlaying}
-						gold={towerDefenseState.gold}
-						onStart={() =>
-							onAction('tower defense', {
-								key: 'tower defense',
-								value: 'start'
-							})
-						}
-						onSelectTower={(towerValue: string) =>
-							onAction('tower defense', {
-								key: 'tower defense',
-								value: 'select tower',
-								tower: towerValue
-							})
-						}
-					/>
-				);
-			case 'background':
-				return (
-					<BackgroundPanel sendImage={(name, type) => onAction(type, name)} />
-				);
-			case 'animation':
-				return <AnimationPanel sendAnimation={onAction} />;
-			case 'whiteboard':
-				return <WhiteboardPanel setBrushColor={setBrushColor} />;
-			case 'settings':
-				return (
-					<SettingsPanel
-						onSubmitUrl={(url) => onAction('settings', 'url', url)}
-						onChangeName={(name) => onAction('settings', 'name', name)}
-					/>
-				);
-			case 'weather':
-				return (
-					<Weather sendLocation={(location) => onAction('weather', location)} />
-				);
-		}
-	};
+	const [images, setImages] = useState<IImagesState[]>([]);
 
 	return (
 		<Drawer variant="persistent" anchor="bottom" open={isOpen}>
 			<div ref={bottomPanelRef} className="bottom-panel-container">
-				{renderPanelContent()}
+				<PanelContent
+					type={type}
+					setBrushColor={setBrushColor}
+					onAction={onAction}
+					towerDefenseState={towerDefenseState}
+					updateIsTyping={updateIsTyping}
+					images={images}
+					setImages={setImages}
+				/>
 			</div>
 		</Drawer>
 	);
+};
+
+const PanelContent = ({
+	type,
+	setBrushColor,
+	onAction,
+	towerDefenseState,
+	updateIsTyping,
+	images,
+	setImages
+}: IPanelContentProps) => {
+	switch (type) {
+		case 'emoji':
+			return (
+				<>
+					{emojiList.map((emoji) => (
+						<div key={emoji} className="bottom-panel-emoji">
+							<IconButton onClick={() => onAction('emoji', emoji)}>
+								{emoji}
+							</IconButton>
+						</div>
+					))}
+				</>
+			);
+
+		case 'chat':
+			return (
+				<Chat
+					sendMessage={(message) => {
+						onAction('chat', message);
+					}}
+					updateIsTyping={updateIsTyping}
+				/>
+			);
+		case 'sound':
+			return <SoundPanel sendSound={onAction} />;
+
+		case 'gifs':
+			return (
+				<Gifs
+					sendGif={(gif: IGif) => {
+						onAction('gif', gif.id);
+					}}
+				/>
+			);
+		case 'tower':
+			return (
+				<TowerDefensePanel
+					isStarted={towerDefenseState.isPlaying}
+					gold={towerDefenseState.gold}
+					onStart={() =>
+						onAction('tower defense', {
+							key: 'tower defense',
+							value: 'start'
+						})
+					}
+					onSelectTower={(towerValue: string) =>
+						onAction('tower defense', {
+							key: 'tower defense',
+							value: 'select tower',
+							tower: towerValue
+						})
+					}
+				/>
+			);
+		case 'background':
+			return (
+				<BackgroundPanel
+					sendImage={(name, type) => onAction(type, name)}
+					setImages={setImages}
+					images={images}
+				/>
+			);
+		case 'animation':
+			return <AnimationPanel sendAnimation={onAction} />;
+		case 'whiteboard':
+			return <WhiteboardPanel setBrushColor={setBrushColor} />;
+		case 'settings':
+			return (
+				<SettingsPanel
+					onSubmitUrl={(url) => onAction('settings', 'url', url)}
+					onChangeName={(name) => onAction('settings', 'name', name)}
+				/>
+			);
+		case 'weather':
+			return (
+				<Weather sendLocation={(location) => onAction('weather', location)} />
+			);
+		default:
+			return null;
+	}
 };
