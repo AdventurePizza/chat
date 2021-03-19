2f65eddfa45b1b599a0e9171849addad8def19d4
Jason Ding <Ventues7@gmail.com>
2021-03-19 11:24:54 -0700

    Simplified the ai prompt and also it takes care of the whole chat dialog

--- client/src/components/OpenAI.tsx
+++ client/src/components/OpenAI.tsx
@@ -5,7 +5,7 @@ import loadingDots from '../assets/loading-dots.gif';
 import openAiPng from '../assets/openAiCharacter.png'
 
 
-const api_key = "API_KEY_HERE";  //process.env.OPENAI_TEST_API_KEY;
+const api_key = "key";  //process.env.OPENAI_TEST_API_KEY;
 const OpenAIAPI = require('openai-api');
 const openai = new OpenAIAPI(api_key);
 
@@ -13,43 +13,33 @@ interface IOpenAIProps {
 	message?:string
 }
 
-const template = "Trybot is a bot who kindly answers questions for this site called trychats.\n"
-                + "###\n"
-                + "User: What is trychats?\n"
-                + "Trybot: trychats is a website for online interactions in forms of gifs, emojis, games, drawings and many more\n"
-                + "###\n"
-                + "User: What game can we play?\n"
-                + "Trybot: we have tower defense game where you can cooperate with other users to defend against waves of enemies\n"
-                + "###\n"
-                + "User: What can we do?\n"
-                + "Trybot: make some drawings, pin background, share with others, make sounds and drawings\n"
+const template = "Trybot is a bot who deliberately chats with others and answers questions for this site called trychats. Trychats is a website for online interactions in forms of gifs, emojis, games, drawings and many more. There is tower defense game where you can cooperate with other users to defend against waves of enemies.\n"
                 + "###\n"
                 + "User: Hello\n"
                 + "Trybot: hi welcome to trychats!\n"
                 + "###\n"
-                + "User: Whats up\n"
-                + "Trybot: hi welcome to trychats!\n"
-                + "###\n"
-                + "User: How do I play games?\n"
-                + "Trybot: click on the tower tab on the left to play\n"
-                + "###\n"
                 + "User: "
-
+let logs = template;
 export const OpenAI = ({message}:IOpenAIProps) => {
     const [isThinking, setIsThinking] = useState(false);
     const [response, setResponse] = useState("");
+    
     useEffect(() => {
 		setIsThinking(true);
+        logs += message + "\nTrybot: ";
         openai.complete({
-            prompt: template + message + "\nTrybot: ",
+            prompt: logs,
             maxTokens: 100,
             n: 3,
             temperature: 0.8,
-            frequencyPenalty: 0,
+            frequencyPenalty: 0.2,
+            presencePenalty: 0.5,
             stop: ["\n", ".", "?", "lol"],
         }).then((response: any) => {
             console.log(response.data.choices)
-            setResponse(response.data.choices[0].text);
+            const index = Math.floor(Math.random()*3)
+            setResponse(response.data.choices[index].text);
+            logs += response.data.choices[index].text + "\n###\nUser: ";
             setIsThinking(false);
         });
     }, [message]);
