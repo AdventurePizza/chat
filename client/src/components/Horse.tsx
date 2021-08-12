dbe569bf690be41d5ef0f09ab9123229ff7739a1
tanerdurmaz <taner.durmaz.cs@gmail.com>
2021-08-13 00:58:35 +0300

    horse view improvements

--- client/src/components/Horse.tsx
+++ client/src/components/Horse.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import { IHorse } from '../types';
+import bloodline from '../assets/horse-view/bloodline.png'
+import genotype from '../assets/horse-view/genotype.png'
+import gender from '../assets/horse-view/gender.png'
+import color from '../assets/horse-view/color.png'
+import race from '../assets/horse-view/race.png'
+import trophy from '../assets/horse-view/trophy.png'
+import hClass from '../assets/horse-view/class.png'
+import offspring from '../assets/horse-view/offspring.png'
+import rating from '../assets/horse-view/rating.png'
+import supercoat from '../assets/horse-view/super-coat.png'
 
 interface IHorseProps {
 	horse: IHorse;
@@ -7,30 +17,42 @@ interface IHorseProps {
 
 export const Horse = ({ horse }: IHorseProps) => {
 
+	let horseSpecs: { title: string, value: string, image: string }[] = [
+		{ "title": "BLOODLINE", "value": horse.bloodline,  "image": bloodline},
+		{ "title": "GENOTYPE", "value": horse.genotype,  "image": genotype},
+		{ "title": "GENDER", "value": horse.horse_type,  "image": gender},
+		{ "title": "COAT COLOR", "value": horse.color,  "image": color},
+		{ "title": "RACE", "value": horse.number_of_races,  "image": race},
+		{ "title": "WIN RATE", "value": horse.win_rate,  "image": trophy},
+		{ "title": "CLASS", "value": horse.class,  "image": hClass},
+		{ "title": "OFFSPRING", "value": horse.breeding_counter,  "image": offspring},
+		{ "title": "RATING", "value": horse.rating,  "image": rating},
+		{ "title": "SUPER COAT", "value": horse.super_coat,  "image": supercoat},
+	];
+
 	return (
-			<div>
-                <div><img alt={horse.name} src={horse.img_url} style={{ width: 180 }} /></div>
-				<div> name: {horse.name} </div>
-				<div> breeding counter: {horse.breeding_counter} </div>
-				<div> breeding_cycle_reset: {horse.breeding_cycle_reset} </div>
-				<div> class: {horse.class} </div>
-				<div> genotype: {horse.genotype} </div>
-				<div> color: {horse.color} </div>
-				<div> hex_code: {horse.hex_code} </div>
-				<div> horse_type: {horse.horse_type} </div>
-				<div> is_approved_for_racing: {horse.is_approved_for_racing} </div>
-				<div> is_in_stud: {horse.is_in_stud} </div>
-				<div> is_on_racing_contract: {horse.is_on_racing_contract} </div>
-				<div> mating_price: {horse.mating_price} </div>
-				<div> number_of_races: {horse.number_of_races} </div>
-				<div> owner: {horse.owner} </div>
-				<div> owner_stable: {horse.owner_stable} </div>
-				<div> owner_stable_slug: {horse.owner_stable_slug} </div>
-				<div> rating: {horse.rating} </div>
-				<div> super_coat: {horse.super_coat} </div>
-				<div> tx: {horse.tx} </div>
-				<div> tx_date: {horse.tx_date} </div>
-				<div> win_rate: {horse.win_rate} </div>
+			<div className='horse-view' >
+				<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontWeight: 'bold', color: '#' + horse.hex_code }}> {horse.name} </div>
+                <div><img alt={horse.name} src={horse.img_url} style={{ width: 180 }} /> </div>
+
+				{horseSpecs.map((h, index) =>
+					<div className='horse-spec' >
+						<img alt={horse.name} src={h.image} style={{ width: 23}}/>
+						<div className= 'horse-spec-text'>
+							<div style={{ fontSize:12, fontWeight: 'bold', color: '#' + horse.hex_code}}> {h.title} </div>
+							<div style={{ fontSize:15 }}>{h.value}</div>
+						</div>
+					</div>	
+				)}
+
+				<div className='horse-spec'  >
+					<a style= {{ fontWeight: 'bold', color: '#' + horse.hex_code}} href={"https://polygonscan.com/address/" + horse.owner} >Owner</a>
+				</div>
+
+				<div className='horse-spec' >
+					<a style= {{ fontWeight: 'bold', color: '#' + horse.hex_code}} href={"https://zed.run/stable/" + horse.owner_stable_slug} >Owner Stable</a>
+				</div>
+				
 			</div>
 	);
 };
